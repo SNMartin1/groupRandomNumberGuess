c5b8dc1fba6155af76d2c5b3ce4895a4a337f3db
Cat Yanish <catyanish@gmail.com>
2017-06-30 10:34:35 -0500

    added variable for player guess, subimt button compares guess to random with for loop and if else

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,14 @@
 console.log("working!");
 var difficulty;
+var targetNumber;
+var player1;
+var player2;
+var player3;
+var player4;
+var guessesArray = [];
+
   $(document).ready(function(){
+
 
 //starting easy mode
 $('#easy-mode').on('click',function(){
@@ -24,25 +32,70 @@ $('#easy-mode').on('click',function(){
       console.log('start click');
       switch (difficulty) {
         case 'easy':
+        randomNumber(1, 10);
         console.log('selected easy mode');
+console.log(targetNumber);
+        break;
 
+        case 'medium':
+        console.log('selected medium mode');
+        randomNumber(1, 100);
+console.log(targetNumber);
+        break;
+        case 'hard':
+        console.log('selected hard mode');
+          randomNumber(1, 10000);
           break;
-          case 'medium':
-console.log('selected medium mode');
-
-            break;
-            case 'hard':
-console.log('selected hard mode');
-
-              break;
         default:
-
+console.log(targetNumber);
       }
 
     });
 
+//console.log(player1);
+//console.log(player2);
+
+$('#submit-guess').on('click', function() {
+    console.log('submit guess worked');
+    player1 = $('#player1').val();
+    player2 = $('#player2').val();
+    player3 = $('#player3').val();
+    player4 = $('#player4').val();
+    console.log(player1);
+    console.log(player2);
+    console.log(player3);
+    console.log(player4);
+    guessesArray.push(player1);
+    guessesArray.push(player2);
+    guessesArray.push(player3);
+    guessesArray.push(player4);
+console.log(guessesArray);
+compare();
+});
+
 
   });//end of document.ready
   function randomNumber(min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);
+    targetNumber = Math.floor(Math.random() * (1 + max - min) + min);
+    //console.log(targetNumber);
+    return targetNumber;
+
+}
+
+function compare() {
+for (var i = 0; i < guessesArray.length; i++) {
+  if (guessesArray[i] == targetNumber ) {
+    console.log('winner winner chicken dinner');
+  } else if (guessesArray[i] > targetNumber) {
+    console.log("guess lower");
+  } else if (guessesArray[i] < targetNumber) {
+    console.log("guess higher");
+  }
+} //end of the for loop if else function
+
+
+
+
+
+
 }
